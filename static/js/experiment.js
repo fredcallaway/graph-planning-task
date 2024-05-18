43dff1975f5e8068cf1c5c75fd0664f83ebd3c47
fredcallaway <fredc@llaway.com>
2024-05-18 07:46:06 -0400

    delete unused blocks

--- static/js/experiment.js
+++ static/js/experiment.js
@@ -217,45 +217,6 @@ async function runExperiment() {
     `)
   }
 
-
-  async function motivation() {
-    DISPLAY.empty()
-    let div = $('<div>').appendTo(DISPLAY).addClass('text')
-    $('<p>').appendTo(div).html(markdown(`
-      # Quick question
-    `))
-
-    let motivation = new Slider({
-      prompt: 'How motivated did you feel to score points?',
-      leftLabel: 'not at all motivated',
-      rightLabel: 'very motivated'
-    }).appendTo(div)
-
-    let speedacc = new Slider({
-      prompt: 'How do you think you balanced <b>speed</b> (doing many rounds) with <b>accuracy</b> (getting the most points possible on every round)?',
-      leftLabel: 'only speed',
-      rightLabel: 'only accuracy'
-    }).appendTo(div)
-
-    // new RadioButtons({
-    //   prompt: 'How motivated did you feel to score points quickly?',
-    //   choices: ['hardly', 'a bit', 'fairly', 'very']
-    // }).appendTo(div)
-
-    await button(div, 'submit').promise()
-    // this information is already in the log, but let's put it in one place
-    logEvent('motivation.submitted', getInputValues({motivation, speedacc}))
-  }
-
-
-  async function survey() {
-    _.shuffle(CLINICAL_SURVEY.pages.slice(0,-1)).forEach((x, i) => {
-      x.elements[0].rows = _.shuffle(x.elements[0].rows)
-      CLINICAL_SURVEY.pages[i] = x
-    })
-    await new SurveyTrial(CLINICAL_SURVEY).run(DISPLAY)
-  }
-
   async function debrief() {
     psiturk.recordUnstructuredData('completed', true)
     DISPLAY.empty()
